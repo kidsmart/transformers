5ce69b5c66e4d0b151261ede41b6bf9afff33d18
Ron Potok <rpotok@gmail.com>
2025-02-13 18:30:01 -0500

    move up crossattention return

--- src/transformers/models/whisper/generation_whisper.py
+++ src/transformers/models/whisper/generation_whisper.py
@@ -868,7 +868,7 @@ class WhisperGenerationMixin(GenerationMixin):
         if return_crossattention:
             # Return the cross attention data without calculating timestamps
             outputs = {
-                "sequences": sequences,
+                "sequences": padded_outputs,
                 "cross_attentions": [output.get("cross_attentions", None) for output in seek_outputs],
                 "alignment_heads": generation_config.alignment_heads if hasattr(generation_config, "alignment_heads") else None,
                 "num_frames": generation_config.get("num_frames", None),
